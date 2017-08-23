50ff6d9c9d21497bb9d7890e76ddee7f0bf3a05a
Vachet Virginie <vachetvirginie@gmail.com>
2017-08-23 12:10:28 +0200

    ajout moteur template mustache

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-var express = require('express');
+const express = require('express');
 let app = express();
-let url = require('url');
+const mustache = require('mustache');
+const fs = require('fs')
 
 app.use(express.static("public"));
+
+app.get("/test", function(req, resp) {
+    let str = mustache.render("hello {{name}}!!!", {
+        name: "vivi"
+    })
+    resp.send(str)
+});
+
+app.engine("html", function(path, option, callback) {
+
+    fs.readFile(path, function(err, content) {
+        if (err) {
+            return callback(err);
+        }
+        let str = mustache.render(content, options);
+        return callback(null, str);
+    })
+
+})
 
 app.listen(80);
